a1fe5ef026a36a442ad5e541352d97d40b0ad7dc
linding3r <thomasjuullindinger@yahoo.dk>
2023-03-04 21:12:19 +0100

    merged

--- day3/day3-ex2.js
+++ day3/day3-ex2.js
@@ -34,11 +34,3 @@ function randomColor(){
     const color = `rgb(${r}, ${g}, ${b})`;
     return color;
 }
-
-function randomColor(){
-    const r = Math.floor(Math.random() * 256);
-    const g = Math.floor(Math.random() * 256);
-    const b = Math.floor(Math.random() * 256);
-    const color = `rgb(${r}, ${g}, ${b})`;
-    return color;
-}
