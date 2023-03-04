fad4ab9f0cc661aab213d5205a8f3c0c6901138d
linding3r <thomasjuullindinger@yahoo.dk>
2023-03-05 00:05:59 +0100

    car by id

--- day3/day3-cars.js
+++ day3/day3-cars.js
@@ -1,5 +1,6 @@
 
 document.getElementById("btn-get-all").onclick = getAllCars
+document.getElementById("btn-for-id").onclick = fetchCar
 
 const URL = "https://lindinger.azurewebsites.net/api/cars"
 
@@ -22,3 +23,27 @@ function makeTable(cars){
 
     document.getElementById("table-body-cars").innerHTML = tableRowsAsString
 }
+
+function fetchCar(evt){
+    const id = document.getElementById("text-for-id").value
+    if(!id){
+     alert("Enter a Car id")
+     return 
+    }
+    document.getElementById("c-error").innerText = ""
+    fetch(URL+"/"+id)
+    .then(res => {
+     if(!res.ok){
+         return Promise.reject("Car Not Found")
+     }
+     return res.json()})
+    .then(data => {
+     document.getElementById("c-id").innerText = data.id;
+     document.getElementById("c-brand").innerText = data.brand;
+     document.getElementById("c-model").innerText = data.model;
+
+    })
+    .catch((error) => {
+     document.getElementById("c-error").innerText = error
+    })
+ }
